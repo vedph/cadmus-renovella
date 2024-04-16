049000d6c851fd66829902760a335e1fdaaac627
Myrmex <dfusi@hotmail.com>
2024-04-16 09:21:19 +0200

    Update Cli.ps1

--- Cli.ps1
+++ Cli.ps1
@@ -1,10 +1,10 @@
 # build
 dotnet build -c Debug
 # publish library
-Remove-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net7.0\publish -Recurse -Force
+Remove-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net8.0\publish -Recurse -Force
 dotnet publish .\Cadmus.Renovella.Services\Cadmus.Renovella.Services.csproj -c Debug
 # rename publish to Cadmus.Renovella.Services and zip
-Rename-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net7.0\publish -NewName Cadmus.Renovella.Services
-compress-archive -path .\Cadmus.Renovella.Services\bin\Debug\net7.0\Cadmus.Renovella.Services\ -DestinationPath .\Cadmus.Renovella.Services\bin\Debug\net7.0\Cadmus.Renovella.Services.zip -Force
+Rename-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net8.0\publish -NewName Cadmus.Renovella.Services
+compress-archive -path .\Cadmus.Renovella.Services\bin\Debug\net8.0\Cadmus.Renovella.Services\ -DestinationPath .\Cadmus.Renovella.Services\bin\Debug\net8.0\Cadmus.Renovella.Services.zip -Force
 # rename back
-Rename-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net7.0\Cadmus.Renovella.Services -NewName publish
+Rename-Item -Path .\Cadmus.Renovella.Services\bin\Debug\net8.0\Cadmus.Renovella.Services -NewName publish
